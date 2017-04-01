275b881ae691a336fe8e9e0594f57e7b04097d8c
Rob McGinley <mcginleyr1@gmail.com>
2017-04-01 13:14:59 -0400

    week2 done no exercises

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+# The notes on python2 are because python 2 distinguishes between ascii and unicode
+# Strings explicitly where as all strings in python 3 are unicode
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.title
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    categories = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': categories}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def show_category(request, category_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict = {
+            'pages': pages,
+            'category': category
+        }
+    except Category.DoesNotExist as e:
+        context_dict = {'pages': None, 'category': None}
+    return render(request, 'rango/category.html', context_dict)
