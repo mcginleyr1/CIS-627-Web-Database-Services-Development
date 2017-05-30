1b27a57dc6d40fc837517ffa6aff92e9e469bfc8
Mike Anderson <andersm3@student.lasalle.edu>
2017-05-30 12:53:23 -0400

    week1

--- week1/tango_with_django_project/tango_with_django_project/wsgi.py
+++ week1/tango_with_django_project/tango_with_django_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tango_with_django_project project.
+WSGI config for tango_with_django_project project..
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
