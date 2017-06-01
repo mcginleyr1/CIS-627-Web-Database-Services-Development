050e34ac994977b673660e51a7baa2c04b063a9a
evenflow22 <vincent.libucki@gmail.com>
2017-06-01 13:48:21 -0400

    week2

--- week2/spidey/dailyplanet/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("My spidey sense is tingling")
-
-def about(request):
-    return HttpResponse("So you want to know all about Spidey, Eh!")
