c29e59ba6e95c1046463aaccf82f31d86739b32c
evenflow22 <vincent.libucki@gmail.com>
2017-06-03 10:51:08 -0400

    week2

--- week2/tango_with_django_project/rango/admin.py
+++ week2/tango_with_django_project/rango/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
 
+admin.site.register(Category, CategoryAdmin)
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category','url')
+
+admin.site.register(Page, PageAdmin)
--- week2/tango_with_django_project/rango/views.py
+++ week2/tango_with_django_project/rango/views.py
@@ -5,6 +5,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	return render(request, 'rango/index.html', context_dict)
+	
+def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
     
