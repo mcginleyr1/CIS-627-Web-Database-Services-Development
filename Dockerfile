985c87c03be417223962520ab26ad26dcf0a1243
jbyrnes <johnbyrnes10@gmail.com>
2017-06-04 22:13:51 -0400

    week2.2

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY requirements.txt requirements.txt
 EXPOSE 8000
 
 RUN pip install -r requirements.txt
+RUN apt-get install libmysqlclient-dev
 
 RUN mkdir /var/djangoproject
 
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'unsecure',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
