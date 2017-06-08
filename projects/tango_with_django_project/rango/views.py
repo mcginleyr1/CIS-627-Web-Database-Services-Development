476afff06fa8695315a03be34bccb49716d5ff82
Howard Stickley <howardstickley12@gmail.com>
2017-06-08 15:09:14 -0400

    Week3
    Chapter 7 - done

--- projects/tango_with_django_project/rango/views.py
+++ projects/tango_with_django_project/rango/views.py
@@ -1,3 +1,5 @@
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
@@ -7,9 +9,11 @@ def index(request):
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
 	return render(request, 'rango/index.html', context_dict)
+
 def about(request):
 	context_dict = {'boldmessage' : 'This tutorial has been put together by Howard Stickley'}
 	return render(request, 'rango/about.html', context=context_dict)
+
 def show_category(request, category_name_slug):
 	context_dict = {}
 	try:
@@ -21,3 +25,36 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None
 		context_dict['pages'] = None
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=True)
+            print(category, category.slug)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+
+	context_dict = {'form':form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
