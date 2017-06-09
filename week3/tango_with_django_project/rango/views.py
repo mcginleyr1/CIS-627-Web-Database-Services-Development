c2b5a1c6f8077f3f0680b54b8746c13554eeda8b
evenflow22 <vincent.libucki@gmail.com>
2017-06-08 21:26:30 -0400

    week3.1b

--- week3/tango_with_django_project/rango/views.py
+++ week3/tango_with_django_project/rango/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
-
+        
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
-	page_list = Page.objects.order_by('-views')[:5]
-	context_dict = {'categories': category_list, 'pages': page_list}
-	return render(request, 'rango/index.html', context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+    return render(request, 'rango/index.html', context_dict)
 def about(request):
-	context_dict = {'boldmessage' : 'This tutorial has been put together by Vincent Libucki'}
-	return render(request, 'rango/about.html', context=context_dict)
+    context_dict = {'boldmessage' : 'This tutorial has been put together by Vincent Libucki'}
+    return render(request, 'rango/about.html', context=context_dict)
 def show_category(request, category_name_slug):
 	context_dict = {}
 	try:
@@ -22,4 +23,16 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None
 		context_dict['pages'] = None
 	return render(request, 'rango/category.html', context_dict)
-	
+    
+def add_category(request)
+    form = CategoryForm()
+
+    if request.method== 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+        return render(request, 'rango/add_category.html', {'form': form})
+            
