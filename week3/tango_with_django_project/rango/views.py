98868d3becc5250303efba3b32aa7df73f45ec07
evenflow22 <vincent.libucki@gmail.com>
2017-06-11 21:20:21 -0400

    week3

--- week3/tango_with_django_project/rango/views.py
+++ week3/tango_with_django_project/rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rango.models import Category
+from django.core.urlresolvers import reverse
+from rango.models import Category, Page
 from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
