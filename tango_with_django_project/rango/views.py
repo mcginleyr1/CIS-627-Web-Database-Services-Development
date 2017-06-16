b466588395871cf43bd0b0b861720b378979d508
Dahlia Moncrieffe <dahliamoncrieffe@gmail.com>
2017-06-15 23:38:04 -0400

    Moncrieffe Week 3  Commit

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 from rango.models import Category
 from rango.models import Page
 from django.http import HttpResponse
+from rango.forms import CategoryForm, PageForm
 
 
 
@@ -37,7 +38,9 @@ def index(request):
 
 #Creating view
 def about(request):
-    return HttpResponse("Rango say this is the about page <br/> <a href='/rango'>Index</a>")
+    print(request.method)
+    print (request.user)
+    return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
@@ -53,6 +56,47 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            cat = form.save(commit=True)
+            print(cat, cat.slug)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request,'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+            return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form':form, 'category': category}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
+
+
+
 
 
 
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -26,8 +26,15 @@ from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
+        #This is how you map to about page
+    url(r'^about/', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+
     url(r'^$',views.index,name= 'index'),
-    url(r'^rango/',include('rango.urls')),
+    url(r'^rango/', include('rango.urls')),
+
 
 
     #Above maps any URLS starting with rango/ to be handled by
