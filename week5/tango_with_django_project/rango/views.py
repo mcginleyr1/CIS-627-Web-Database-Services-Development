c21e7713bfbbbe431c3f965c2b3f4bbb3c81d7da
evenflow22 <vincent.libucki@gmail.com>
2017-06-21 08:58:07 -0400

    week5

---
+++ week5/tango_with_django_project/rango/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+
+def index(request):
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}
+	return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+	return render(request, 'rango/about.html', {})
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=True)
+            print(category, category.slug)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+         user = user_form.save()
+
+         user.set_password(user.password)
+         user.save()
+
+         profile = profile_form.save(commit=False)
+
+         profile.user = user
+
+
+        if 'picture' in request.FILES:
+            profile.picture = request.FILES['picture']
+
+        profile.save()
+
+        registered = True
+
+    else:
+
+        print(user_form.errors, profile_form.errors)
+
+else:
+
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+
+return render(request,
+                'rango/register.html',
+                {'user_form': user_form,
+                'profile_form': profile_form,
+                'registered': registered})
