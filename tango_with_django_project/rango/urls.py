97e4a090bd06ba818d75adc93156e9bb279d8d71
Rich Abreu <rich.abreu@gmail.com>
2017-06-22 22:26:27 -0400

    Week 3 - late entry

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,16 @@
+#from django.conf.urls import patterns, url
+from django.conf.urls import url
+from rango import views
+
+#urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
+    url(r'(?i)^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+]
