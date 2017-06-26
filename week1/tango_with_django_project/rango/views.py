dcb4e501c6e38df54d87b821d19686b0525addcd
manderson727 <manderson_311@yahoo.com>
2017-06-26 19:52:57 -0400

    Week5 Chap10

--- week1/tango_with_django_project/rango/views.py
+++ week1/tango_with_django_project/rango/views.py
@@ -9,23 +9,38 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 #def index(request):
 #    return HttpResponse("Rango says hey there partner! Visit the <a href='rango/about'>About</a> page")
 
 def index(request):
+        request.session.set_test_cookie()
         #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
         #return render(request, 'rango/index.html', context=context_dict)
         category_list = Category.objects.order_by('-likes')[:5]
         page_list = Page.objects.order_by('-views')[:5]
         context_dict = {'categories': category_list, 'pages': page_list}
 
-        return render(request, 'rango/index.html', context_dict)
+        #visitor_cookie_handler(request, response)
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+
+        response = render(request, 'rango/index.html', context_dict)
+        #return render(request, 'rango/index.html', context_dict)
+        return response
 
 def about(request):
     #return HttpResponse("Rango says there is an about page! Go back to the <a href='/'>Home</a> page")
     #context_dict = {}
-    return render(request, 'rango/about.html', {})
+    #if request.session.test_cookie_worked():
+    #    print("TEST COOKIE WORKED!")
+    #    request.session.delete_test_cookie()
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    #return render(request, 'rango/about.html', {})
+    return render(request, 'rango/about.html', context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -142,3 +157,39 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+#def visitor_cookie_handler(request, response):
+#    visits = int(request.COOKIES.get('visits', '1'))
+#
+#    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+#    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+#
+#    #if(datetime.now() - last_visit_time).days > 0:
+#    if(datetime.now() - last_visit_time).seconds > 0:
+#        visits = visits + 1
+#        response.set_cookie('last_visit', str(datetime.now()))
+#    else:
+#        visits = 1
+#        response.set_cookie('last_visit', last_visit_cookie)
+#
+#    response.set_cookie('visits', visits)
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
